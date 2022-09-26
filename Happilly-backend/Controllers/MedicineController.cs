0f420658a5ac31ac2c315d025cf45b90fb5d4698
yannickts <yannick.tsang@hotmail.com>
2022-09-26 22:10:58 +0200

    Initial setup + gitignore

---
+++ Happilly-backend/Controllers/MedicineController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Happilly_backend.Models;
+
+namespace Happilly_backend.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MedicineController : Controller
+    {
+        public Medicine medicine = new();
+        
+        [HttpGet]
+        public JsonResult GetMedicine()
+        {
+            List<Medicine> medicines = new();
+            Medicine medicine1 = new();
+            medicine1.MedicineId = 1;
+            medicine1.MedicineName = "Paracetamol";
+            medicine1.MedicineDescription = "Paracetamol, also known as acetaminophen, is a medication used to treat fever and mild to moderate pain.";
+            medicines.Add(medicine1);
+            return Json(medicines);
+        }
+    }
+}
---
+++ Happilly-backend/Models/Medicine.cs
@@ -0,0 +1,21 @@
+﻿namespace Happilly_backend.Models
+{
+    public class Medicine
+    {
+        public int MedicineId { get; set; }
+        public string MedicineName { get; set; }
+        public string MedicineDescription { get; set; }
+
+        public Medicine()
+        {
+
+        }
+
+        public Medicine(Medicine medicine)
+        {
+            MedicineId = medicine.MedicineId;
+            MedicineName = medicine.MedicineName;
+            MedicineDescription = medicine.MedicineDescription;
+        }
+    }
+}
