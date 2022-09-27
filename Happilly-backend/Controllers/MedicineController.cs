0c8dcbd0af1e1a402328c5578f1d8408b5a028eb
yannickts <yannick.tsang@hotmail.com>
2022-09-27 02:20:28 +0200

    added medicine

--- Happilly-backend/Controllers/MedicineController.cs
+++ Happilly-backend/Controllers/MedicineController.cs
@@ -18,6 +18,16 @@ namespace Happilly_backend.Controllers
             medicine1.MedicineName = "Paracetamol";
             medicine1.MedicineDescription = "Paracetamol, also known as acetaminophen, is a medication used to treat fever and mild to moderate pain.";
             medicines.Add(medicine1);
+            Medicine medicine2 = new();
+            medicine2.MedicineId = 2;
+            medicine2.MedicineName = "Adderall";
+            medicine2.MedicineDescription = "Adderall and Mydayis are trade names for a combination drug called mixed amphetamine salts containing four salts of amphetamine.";
+            medicines.Add(medicine2);
+            Medicine medicine3 = new();
+            medicine3.MedicineId = 3;
+            medicine3.MedicineName = "Ibuprofen";
+            medicine3.MedicineDescription = "Ibuprofen is a nonsteroidal anti-inflammatory drug that is used for treating pain, fever, and inflammation. This includes painful menstrual periods, migraines, and rheumatoid arthritis.";
+            medicines.Add(medicine3);
             return Json(medicines);
         }
     }
